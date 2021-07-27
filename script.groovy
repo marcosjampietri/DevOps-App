20bf8cfc8465356acb84a13a8f6417adec70a651
MARCOS JAMPIETRI <marcosjampietri@marcoss-macbook-pro.local>
2021-07-27 19:22:52 +0000

    change docjer image tag

--- script.groovy
+++ script.groovy
@@ -9,13 +9,13 @@ def testApp() {
 def buildImage() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker_credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t marcosjampietri/Kajabi_client ./client'
-        sh 'docker build -t marcosjampietri/Kajabi_api ./server'
+        sh 'docker build -t marcosjampietri/devops_client ./client'
+        sh 'docker build -t marcosjampietri/devops_api ./server'
         
         sh "echo $PASS | docker login -u $USER --password-stdin"
         
-        sh 'docker push marcosjampietri/Kajabi_client'
-        sh 'docker push marcosjampietri/Kajabi_api'
+        sh 'docker push marcosjampietri/devops_client'
+        sh 'docker push marcosjampietri/devops_api'
     }
 } 
 
