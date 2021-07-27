1b29e539ad5feaabef877ce07fbfc549274045ce
MARCOS JAMPIETRI <marcosjampietri@marcoss-macbook-pro.local>
2021-07-27 15:09:54 +0000

    webhook test

--- script.groovy
+++ script.groovy
@@ -1,19 +1,25 @@
+#!/usr/bin/env groovy
+
 def buildJar() {
-echo "building the application..."
-sh 'mvn package'
+    echo "building the application..."
+    sh 'mvn package'
 } 
 
 def buildImage() {
-echo "building the docker image..."
-withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-    sh 'docker build -t nanajanashia/demo-app:jma-2.0 .'
-    sh "echo $PASS | docker login -u $USER --password-stdin"
-    sh 'docker push nanajanashia/demo-app:jma-2.0'
-}
+    echo "building the docker image..."
+    withCredentials([usernamePassword(credentialsId: 'docker_credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh 'docker build -t marcosjampietri/Kajabi_client ./client'
+        sh 'docker build -t marcosjampietri/Kajabi_api ./server'
+        
+        sh "echo $PASS | docker login -u $USER --password-stdin"
+        
+        sh 'docker push marcosjampietri/Kajabi_client'
+        sh 'docker push marcosjampietri/Kajabi_api'
+    }
 } 
 
 def deployApp() {
-echo 'deploying the application...'
+    echo 'deploying the application...'
 } 
 
 return this
